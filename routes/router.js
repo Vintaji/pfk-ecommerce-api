a44296bf4d6c5779f00b86d6b412198a81392e23
Vintaji <72357630+Vintaji@users.noreply.github.com>
2021-10-14 09:20:41 -0300

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -19,14 +19,3 @@ router.get('/charges/cancelation/:id', charges.cancelationCharge);
 router.get('/charge', charges.charge);
 
 module.exports = router;
-/* const fs = require('fs');
-const path = require('path');
-
-module.exports = server => {
-    const files = fs.readdirSync(__dirname)
-    const filesFilter = files.filter(file => ((file.indexOf('.')) !== 0 && (file !== 'router.js')))
-    filesFilter.forEach(file => {
-        const filePath = path.resolve(__dirname, file)
-        require(filePath)(server)
-    })
-} */
