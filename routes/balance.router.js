cd8493c70f10465c0f7995227fcc3ae59b19fd35
Vintaji <rick_salvador@hotmail.com>
2021-11-04 16:04:38 -0300

    teste

--- routes/balance.router.js
+++
@@ -1,7 +0,0 @@
-/* const express = require('express');
-const router = express.Router();
-
-const { getUserBalance } = require('../controllers/balance.controller');
-router.get('/balance', getUserBalance);
-
-module.exports = router; */
